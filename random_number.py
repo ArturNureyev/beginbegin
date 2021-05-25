5c7bb48b35c0b43e336b9028315341322ca521d6
ArturNureyev <65730577+ArturNureyev@users.noreply.github.com>
2021-05-26 00:37:27 +0300

    Add files via upload
    Возможность сыграть еще раз и выбрать диапазон цисла

--- random_number.py
+++ random_number.py
@@ -2,17 +2,20 @@ import random
 def random_num(from_num, to_num):
     return random.randint(from_num, to_num)
 secret_num = random_num(1, 100)
+counter = 0
+right_border = 100
 print("Добро пожаловать в числовую угадайку")
 def is_valid_num(users_num):
-    if users_num.isdigit() and  0 < int(users_num) < 101:
+    if users_num.isdigit() and  0 < int(users_num) < right_border + 1:
         return True
     else:
         return False
 while True:
-    print("Веедите число от 1 до 100(включительно)")
+    print("Введите число от 1 до", right_border, "(включительно)")
     num = input()
+    counter += 1
     if is_valid_num(num) is False:
-        print('А может введем корректное чило от 1 до 100?')
+        print('А может введем корректное чило от 1 до', right_border)
         continue
     else:
         num = int(num)
@@ -23,6 +26,20 @@ while True:
             print("Ваше число больше загаданного, попробуйте еще разок")
             continue
         elif num == secret_num:
-            print("Вы угадали, поздравляем!")
-            break
+            print("Вы угадали, поздравляем!\nВаше количество попыток:", counter)
+            print("Если хотите сыграть еще, напишите 'да'")
+            if input().lower() == "да":
+                print("Если хотите расширить диапазон числа, напишите 'да'")
+                if input().lower() == "да":
+                    print("Введите крайнюю правую границу диапазона")
+                    right_border = int(input())
+                    secret_num = random_num(1, right_border)
+                    counter = 0
+                    continue
+                else:
+                    secret_num = random_num(1, 100)
+                    counter = 0
+                    continue
+            else:
+                break
 print("Спасибо, что играли в числовую угадайку. Еще увидимся...")
